f20a0fd96c02dac34b652494b7bdf663b63356ea
junichiro <j1okabe@gmail.com>
2022-01-10 10:38:29 +0900

    master

---
+++ src/settings.h
@@ -0,0 +1,20 @@
+/** Copyright 2022 < j1okabe >  */
+
+#pragma once
+
+namespace WiFiInfo {
+constexpr auto SSID = "[your ssid]";
+constexpr auto PASS = "[your key]";
+}  // namespace WiFiInfo
+
+namespace WiFiInfo2 {
+constexpr auto SSID = "[youre ssid2]";
+constexpr auto PASS = "[your key]";
+}  // namespace WiFiInfo2
+
+namespace AmbientInfo {
+// your Channel ID for Ambient
+constexpr auto AMB_CH_ID = 12345;
+// your Write Key to Channel
+constexpr auto AMB_WR_KEY = "[your key]";
+}  // namespace AmbientInfo
